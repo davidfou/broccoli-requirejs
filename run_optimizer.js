8a8d49238c7b12ee4029c7a7b7e8f2c2d774488c
pieter-v <d1ntpanic@gmail.com>
2015-10-06 13:48:02 +0200

    process.send is async (see: https://github.com/nodejs/node/issues/760)
    add test for async behaviour of process.send

--- run_optimizer.js
+++ run_optimizer.js
@@ -1,20 +1,18 @@
 var requirejs = require('requirejs');
 
 function success(buildResponse) {
-  process.send({
+  sendAndExit({
     isSuccess: true,
     output: buildResponse
   });
-  process.exit();
 }
 
 function error(err) {
   err = err || {};
-  process.send({
+  sendAndExit({
     isSuccess: false,
     output: err.message
   });
-  process.exit();
 }
 
 process.on('message', function(m) {
@@ -22,3 +20,15 @@ process.on('message', function(m) {
   eval(m); // jshint ignore:line
   requirejs.optimize(options, success, error);
 });
+
+function sendAndExit(data) {
+  var major = process.versions.node.match(/\d+/)[0];
+  if (major >= 4) {
+    process.send(data, function() {
+      process.exit();
+    });
+  } else {
+    process.send(data);
+    process.exit();
+  }
+}
---
+++ test/fixtures/subfolder-with-a-long-folder-name-1/subfolder-with-a-long-folder-name-2/subfolder-with-a-long-folder-name-b/long_name_to_create_a_larger_response17.js
@@ -0,0 +1,3 @@
+define(function() {
+  return 'long_name_to_create_a_larger_response17';
+});
